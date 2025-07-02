317d1d71167c942d9afd4ab2585e27635705b91a
Chandrika9149 <154324131+Chandrika9149@users.noreply.github.com>
2025-07-02 10:55:59 +0530

    Create Document.java

---
+++ Document.java
@@ -0,0 +1,12 @@
+<employees>
+    <employee>
+        <id>101</id>
+        <name>John Doe</name>
+        <department>Finance</department>
+    </employee>
+    <employee>
+        <id>102</id>
+        <name>Jane Smith</name>
+        <department>IT</department>
+    </employee>
+</employees>
